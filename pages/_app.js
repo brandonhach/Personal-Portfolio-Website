b31372e6fd7b84490d1bb0b93cc01ff7fc87ce66
brandonhach <specialcambo@gmail.com>
2023-08-06 14:56:45 -0400

    update responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 // pages/_app.js
 
 import "../styles/globals.css";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			</Head>
 			<Component {...pageProps} />
 		</>
 	);
